37f3b433f588cfb67c0e98310e71533b38588715
Your Name <you@example.com>
2023-10-05 03:32:18 +0400

    first commit

---
+++ 04.10/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _04._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+       
+        //Task2     
+        //{
+        //    Area(5);
+
+        //    Area(4, 8);
+
+        //    Area(3, 5, 7);
+
+        //    Area(4, 6, 8, 4);
+
+        //}
+        //public static void Area( int r)
+        //{
+        //    int p = 3;
+        //    Console.WriteLine( "Cevre S = "+p*(r*r));
+        //}
+        //public static void Area(int a, int b)
+        //{
+        //    Console.WriteLine("Duzbucaqli S = " + (a * b));
+        //}
+        //public static void Area(int a, int b, int c)
+        //{
+        //    Console.WriteLine(  " Duzbucaqli paralelepiped S = " + 2*(a*b+a*c+b*c));
+        //}
+        //public static void Area(int a, int b, int c, int r)
+            
+        //{
+        //    int p = (a + b + c) / 2;
+           
+        //    Console.WriteLine(" Ucbucaq daxili cevre S = " + p*r);
+        //}
+
+        
+        //Task1
+
+        //{
+        //    int[] numbers = { 1, 2, 3, 4, 5, };
+
+        //    Console.WriteLine(GetMin(numbers));
+        //}
+        //public static int GetMin(int[] arr)
+        //{ int min = arr[0];
+
+        //        for (int i = 1; i < arr.Length; i++)
+        //    {
+        //        if (arr [i]<min)
+        //        {
+        //            min=arr[i];
+        //        }  
+        //    }
+        //    return min;  
+    }
+       
+       
+    
+}
